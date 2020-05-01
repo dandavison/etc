f5920db3321f491067199a3dbe79e5fe8d31e17c
Dan Davison <dandavison7@gmail.com>
2020-05-01 08:46:45 -0400

    delta-140-test

--- file.py
+++ file.py
@@ -1,6 +1,5 @@
      savannah_data = ecosystems.savannah_data
      giraffe_dob = parser.parse(savannah_data["giraffe"]["date_of_birth"]).date()
-     first_name = savannah_data["giraffe"]["first_name"]
-     last_name = savannah_data["giraffe"]["last_name"]
-     giraffe_name = f"{first_name} {last_name}"
+     giraffe_first_name = savannah_data["giraffe"]["first_name"]
+     giraffe_last_name = savannah_data["giraffe"]["last_name"]
      eagle_id = get_eagle_id()
